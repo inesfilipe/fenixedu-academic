c175d34e643d1f4d6f8c846c56e2f071cd055060
Inês Filipe <ines.filipe@tecnico.ulisboa.pt>
2019-09-13 09:32:58 +0100

    added script to add new degree classification

---
+++ src/main/java/org/fenixedu/academic/task/AddNewDegreeClassification.java
@@ -0,0 +1,14 @@
+package org.fenixedu.academic.task;
+
+import org.fenixedu.academic.domain.raides.DegreeClassification;
+import org.fenixedu.bennu.scheduler.custom.CustomTask;
+
+public class AddNewDegreeClassification extends CustomTask {
+
+    @Override
+    public void runTask() throws Exception {
+        new DegreeClassification("T", "Curso técnico superior profissional", "Curso técnico superior profissional", "CTeSP");
+
+    }
+
+}
